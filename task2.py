c3f4a4657d3e5190ca670ea9229e442ebf1eeb56
Kirill <51190223+CINCINATI333@users.noreply.github.com>
2020-02-08 00:47:34 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -13,17 +13,10 @@ for k in range(len(ipset)):
     freq.append(ip.count(ipset[k]))
 freq.sort()
 freq.reverse() # - теперь этот массив отсортирован по убыванию
-for k in range(len(ipset)):
-    if ip.count(ipset[k]) == freq[0]: # если встречаем самый популярный массив, то вписываем его в ответ
-        a.append(ipset[k])
-    elif ip.count(ipset[k]) == freq[1]: # если встречаем второй по популярности массив, вписываем в ответ (и т.д.)
-        a.append(ipset[k])
-    elif ip.count(ipset[k]) == freq[2]:
-        a.append(ipset[k])
-    elif ip.count(ipset[k]) == freq[3]:
-        a.append(ipset[k])
-    elif ip.count(ipset[k]) == freq[4]:
-        a.append(ipset[k])
+for i in range(5):
+    for k in range(len(ipset)):
+        if ip.count(ipset[k]) == freq[i]: # если встречаем самый популярный массив, то вписываем его в ответ
+            a.append(ipset[k])
 a = set(a) # для исключения повторов, при этом ip-адреса не будут расположены по убыванию частоты вхождений, но этого в задании и не требуется
 print('Answer :','\n',a)  
 #ans ['154.157.157.156'] ['82.146.232.163'] ['226.247.119.128', '194.78.107.33'] ['21.143.243.182'] - это ip адреса, расположенные по убыванию частоты вхождений
